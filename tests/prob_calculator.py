971279f4334ff602e265f6544aa56e388f9e6521
creinis <115181527+creinis@users.noreply.github.com>
2024-08-05 12:00:23 +0200

    feat: Hat method should remove balls at random from contents and return those balls as a list of strings. The balls should not go back into the hat during the draw, similar to an urn experiment without replacement. If the number of balls to draw exceeds the available quantity

--- tests/prob_calculator.py
+++ tests/prob_calculator.py
@@ -4,5 +4,13 @@ class Hat:
     def draw(self, num):
         balls = []
         
+        for _ in range(num):
+            try:
+                ball = random.choice(self.contents)
+                self.contents.remove(ball)
+                balls.append(ball)
+            except IndexError:
+                break
+            
         return balls
 
